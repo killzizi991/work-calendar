aeaf17914096cdbfe5a78453c442d55035bdc581
killzizi991 <killzizi@yandex.ru>
2025-09-08 19:39:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,38 @@
-const CACHE_NAME = 'sales-calendar-v4';
+const CACHE_NAME = 'calendar-app-v4';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './icon-192.png',
-  './icon-512.png'
+  './manifest.json',
+  './icon-192.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
